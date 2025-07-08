f1bfea945c31f7d91964f39bbe77de85b7fe6e4b
CharaD7 <jijakahn6@gmail.com>
2025-07-08 03:23:14 +0000

    Initialized commands

--- lua/jetbrainsai/init.lua
+++ lua/jetbrainsai/init.lua
@@ -4,6 +4,7 @@ function M.setup(opts)
   require("jetbrainsai.config").load(opts)
   require("jetbrainsai.proxy").check_proxy()
   require("jetbrainsai.ui").init()
+  require("jetbrainsai.commands").init()
 end
 
 return M
